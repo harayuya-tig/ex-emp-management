cdbcd7b2d5910b11d0b4c8b363d3a7358b5c8f50
Hara Yuya <harayuya.liamg@gmail.com>
2024-11-12 14:37:20 +0900

    AdministratorControllerのinsertメソッドの実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,17 +1,47 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
 
 @Controller
 @RequestMapping("")
 public class AdministratorController {
     
+    @Autowired
+    private AdministratorService administratorService;
+
+    /**
+     * 管理者情報登録画面へフォワードする
+     * @param form
+     * @return 管理者情報登録画面のviewへのパス
+     */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+    }
+
+    /**
+     * 管理者情報を登録する
+     * @param form
+     * @return ログイン画面にリダイレクト
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator administrator = new Administrator();
+        
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+
+        administratorService.insert(administrator);
+
+        return "/";
     }
 }
